92d90fa02fde0a33c307471c4825d9d6398ba0c3
Paul Rail <paul.a.rail@gmail.com>
2017-07-25 12:22:09 -0400

    Setting up the repository for this course

---
+++ Part 1 - Server Test/index.js
@@ -0,0 +1,7 @@
+// console.log("Hello from JavaScript in another file!");
+
+console.log(document);
+console.log(typeof document);
+// this shows us that document is actually an "object"
+
+document.getElementById('example').innerHTML = "This text has been replaced with JS!";
