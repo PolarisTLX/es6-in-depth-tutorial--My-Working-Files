ca0ec26b18ed565561750e23570fb3a9164e5cf0
Paul Rail <paul.a.rail@gmail.com>
2017-07-26 02:12:07 -0400

    Reviewing some basics

--- Part 1 - Server Test/index.js
+++ Part 1 - Server Test/index.js
@@ -5,3 +5,19 @@ console.log(typeof document);
 // this shows us that document is actually an "object"
 
 document.getElementById('example').innerHTML = "This text has been replaced with JS!";
+
+
+
+
+// object example practice:
+
+let dog = {
+  name: "Buddy",
+  breed: "Golden Retreiver",
+  weight: 60,
+  bark: function() {
+    console.log("Woof!");
+  }
+};
+
+dog.bark();    //this consoles "Woof!"
